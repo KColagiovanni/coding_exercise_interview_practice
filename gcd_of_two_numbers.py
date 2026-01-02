6b06842ac63034ab21bd2413f770fead7f1d8057
Kevin Colagiovanni <76464469+KColagiovanni@users.noreply.github.com>
2026-01-01 22:50:07 -0800

    Completed the gcd program.

--- gcd_of_two_numbers.py
+++ gcd_of_two_numbers.py
@@ -1,5 +1,11 @@
 def gcd(a, b):
-    return
+    denominator = 0
+
+    while True:
+        denominator += 1
+        if denominator % a == 0 and denominator % b == 0:
+            break
+    print(f'The GCD of {a} and {b} is {denominator}')
 
 if __name__ == '__main__':
-    gcd(4, 5)
+    gcd(23764, 21349)
