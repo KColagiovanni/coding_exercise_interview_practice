5f2818d6865da21f83b797b1e7837efcef2dacd5
Kevin Colagiovanni <76464469+KColagiovanni@users.noreply.github.com>
2026-01-02 21:31:48 -0800

    Completed the non-pythonic way in palindrome_checker.py which was just taking the non-pythonic functionality from reverse_string.py.

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -9,11 +9,19 @@ word_list = [
 
 def is_it_a_palindrome(word):
     """ Check if a word is the same forwards and backwards """
-    if word == word[::-1]:  # Pythonic
+
+    # Non-Pythonic way.
+    rev_string = ''
+    for char in range(len(word)):
+        last_char = word[-1 - char]
+        rev_string += last_char
+
+    # if word == word[::-1]:  # Pythonic
+    if word == rev_string:
         print(f'{word} is a palindrome')
     else:
         print(f'"{word}" is NOT a palindrome')
 
 if __name__ == '__main__':
-    for word in word_list:
-        is_it_a_palindrome(word)
+    for phrase in word_list:
+        is_it_a_palindrome(phrase)
