c5a82e7cebe937c77a1b16bc613b315824f776c6
Kevin Colagiovanni <76464469+KColagiovanni@users.noreply.github.com>
2026-01-08 22:15:43 -0800

    Added a variable for the fake data input delay.

--- sensor_data_validation.py
+++ sensor_data_validation.py
@@ -19,32 +19,32 @@ import time
 
 sample_counter = []
 
-def validate_data(timestamp, data_value, min_tolerance, max_tolerance, n_samples):
+def validate_data(timestamp, data_value, min_tol, max_tol, n_sample):
     """
 
     :param timestamp: The timestamp of the data.
     :param data_value: The value of the data.
-    :param min_tolerance: The minimum allowable tolerance for the data value.
-    :param max_tolerance: The maximum allowable tolerance for the data value.
-    :param n_samples: The number of consecutive samples that can exceed an acceptable range for more than N samples.
+    :param min_tol: The minimum allowable tolerance for the data value.
+    :param max_tol: The maximum allowable tolerance for the data value.
+    :param n_sample: The number of consecutive samples that can exceed an acceptable range for more than N samples.
     :return: (list) [(str) result: PASS/FAIL, (list) failed timestamps]
     """
     tolerance = 'In-Range'
-    if data_value < min_tolerance or data_value > max_tolerance:
+    if data_value < min_tol or data_value > max_tol:
         sample_counter.append(timestamp)
-        if data_value > max_tolerance:
+        if data_value > max_tol:
             tolerance = 'TOO HIGH'
-        elif data_value < min_tolerance:
+        elif data_value < min_tol:
             tolerance = 'TOO LOW'
         else:
             tolerance = "In-Range"
     print(f'Output - Timestamp:{timestamp} | Data: {data_value}({tolerance})')
-    if len(sample_counter) == n_samples:
+    if len(sample_counter) == n_sample:
         out_of_tolerance_counter = 0
         for sample_index in range(1, len(sample_counter)):
             if sample_counter[sample_index - 1] == sample_counter[sample_index] - 1:
                 out_of_tolerance_counter += 1
-        if out_of_tolerance_counter == n_samples - 1:
+        if out_of_tolerance_counter == n_sample - 1:
             return ['FAIL', sample_counter]
         sample_counter.pop(0)
 
@@ -52,12 +52,13 @@ def validate_data(timestamp, data_value, min_tolerance, max_tolerance, n_samples
 
 if '__main__' == __name__:
 
-    data_samples = 1000  # Number of times to create "fake" data.
+    data_samples = 100  # Number of times to create "fake" data.
     min_range = 40  # Lower range for the "fake" data.
     max_range = 60  # Upper range for the "fake" data.
     min_tolerance = 45  # Lower value for the tolerance.
     max_tolerance = 55  # Upper value for the tolerance.
-    n_samples = 10  # Number of consecutive samples that can exceed an acceptable range.
+    n_samples = 5  # Number of consecutive samples that can exceed an acceptable range.
+    delay = .01 # Seconds
 
     # Create "fake" data
     for count in range(data_samples):
@@ -75,4 +76,4 @@ if '__main__' == __name__:
         if test_result[0] == 'FAIL':
             print(f'Results: {test_result[0]} | (Timestamps: {test_result[1]})')
             break
-        time.sleep(.1)  # Simulate data delay
+        time.sleep(delay)  # Simulate data delay
